546423d074d524c82d19cdebcaf4b30fd4af864b
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-06-13 16:43:05 +0200

    Created a rgb color form

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("Hello");
+const form = document.querySelector(`#convertForm`);
+const rgbDisplay = document.querySelector(`p`);
+
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+  const r = form.elements.red.value;
+  const g = form.elements.green.value;
+  const b = form.elements.blue.value;
+  rgbDisplay.innerText = `Your color: RGB(${r}, ${g}, ${b})`;
+});
